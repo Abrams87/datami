4b381901a4f52cc72ccc5f15186d546f9577caa6
luceroproject.daquin <luceroproject.daquin@gmail.com>
2012-03-08 09:47:57 +0000

    clean up

--- datami-proxy/run-fuseki.sh
+++ datami-proxy/run-fuseki.sh
@@ -1,3 +1,6 @@
+# example script to run Fuseki to be used alongside datami-proxy
+# assumes fuseki has been downloaded and copied in Fuseki-0.2.0
+
 cd Fuseki-0.2.0/
 ./fuseki-server --update --port=3031 --loc=../repository/ /wll
 cd -
